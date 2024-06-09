abd1bd32cc16d70464d63aca40fbbc445ec0144c
to4to <justjustifying@outlook.com>
2024-06-09 22:30:53 +0530

    refactor: Update anchor/Cargo.lock with new dependencies

--- anchor/programs/crud-dapp/src/lib.rs
+++ anchor/programs/crud-dapp/src/lib.rs
@@ -6,65 +6,18 @@ declare_id!("JhhrH8ufVyDTXer786454mvyixLr6wEEfnEBe5QjS4E");
 
 #[program]
 pub mod crud_dapp {
-    use super::*;
+    use     super::*;
 
-  pub fn close(_ctx: Context<CloseCrudDapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.crud_dapp.count = ctx.accounts.crud_dapp.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.crud_dapp.count = ctx.accounts.crud_dapp.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeCrudDapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.crud_dapp.count = value.clone();
-    Ok(())
-  }
-}
-
-#[derive(Accounts)]
-pub struct InitializeCrudDapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + CrudDapp::INIT_SPACE,
-  payer = payer
-  )]
-  pub crud_dapp: Account<'info, CrudDapp>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseCrudDapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub crud_dapp: Account<'info, CrudDapp>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub crud_dapp: Account<'info, CrudDapp>,
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct CrudDapp {
-  count: u8,
+pub struct  JournalEntryState{
+
+    pub owner:Pubkey,
+    #[max_len(20)]
+    pub title: String,
+    #[max_len(200)]
+    pub message: String,
+    pub entry_id: u64,
 }
